86333882b224a5b011706e5713085b1452bbfa91
Clint Batte <clint.batte@gmail.com>
2019-09-04 01:28:07 +0000

    check in pagingation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 
     def index
         # @places = Place.all
-        @pagy, @places = pagy(Place.all)
+        @pagy, @places = pagy(Place.all, items: 5)
 
     end
 
