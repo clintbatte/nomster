4c89d2038f95bfc8d2224d0c8eb149c0c0036e95
Clint Batte <clint.batte@gmail.com>
2019-09-05 02:28:23 +0000

    adding in controller for forms

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,24 @@
 class PlacesController < ApplicationController
 
     def index
-        # @places = Place.all
+        @places = Place.all
         @pagy, @places = pagy(Place.all, items: 5)
 
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        Place.create(place_params)
+        redirect_to root_path
+    end
+    
+    private
+  
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
     end
 
 end
