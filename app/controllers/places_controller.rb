15d851ecbd6120f6e5fcd79425d6dfd948644020
Clint Batte <clint.batte@gmail.com>
2019-09-06 14:32:49 +0000

    adding user place input

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-
+    before_action :authenticate_user!, only: [:new, :create]
+    
     def index
         @places = Place.all
         @pagy, @places = pagy(Place.all, items: 5)
@@ -11,7 +12,8 @@ class PlacesController < ApplicationController
     end
 
     def create
-        Place.create(place_params)
+        #Place.create(place_params)
+        current_user.places.create(place_params)
         redirect_to root_path
     end
     
