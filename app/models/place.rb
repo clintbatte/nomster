e6c24886bad504c895465ff1ad5476f5e992ceb8
Clint Batte <clint.batte@gmail.com>
2019-09-22 00:14:28 +0000

    undefined method name for nil:NilClass

--- app/models/place.rb
+++ app/models/place.rb
@@ -5,6 +5,7 @@ class Place < ApplicationRecord
     geocoded_by :address
     after_validation :geocode
     
-    validates :name, length: { minimum: 3 }
-    validates :address, :description, presence: true
+    validates :name, presence: true, length: {minimum: 3}
+    validates :address, presence: true
+    validates :description, presence: true
 end
