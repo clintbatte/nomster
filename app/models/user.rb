c8a2057add590f029d4f6640af2cee24b35d6b3f
Clint Batte <clint.batte@gmail.com>
2019-09-27 20:01:42 +0000

    adding back dependant: :destroy in place.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :places, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :photos, dependent: :destroy
+  has_many :places
+  has_many :comments
+  has_many :photos
 end
